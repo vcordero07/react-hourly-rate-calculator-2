02ada83ba169b807fc8531f1048c7d2596acc8ac
vcordero07 <vcordero07@gmail.com>
2018-01-11 22:47:37 -0500

    wip

--- src/components/number-input.js
+++ src/components/number-input.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React from "react";
 
 export default function NumberInput(props) {
-    return (
-        <div className="form-group">
-            <label htmlFor={props.id}>{props.label}</label>
-            <input type="number" id={props.id} min={props.min} max={props.max} />
-        </div>
-    );
+  return (
+    <div className="form-group">
+      <label htmlFor={props.id}>{props.label}</label>
+      <input
+        type="number"
+        onChange={props.onChange}
+        value={props.value}
+        id={props.id}
+        min={props.min}
+        max={props.max}
+      />
+    </div>
+  );
 }
--- src/components/rate-calculator.js
+++ src/components/rate-calculator.js
@@ -1,22 +1,61 @@
-import React from 'react';
+import React from "react";
 
-import NumberInput from './number-input';
-import Output from './output';
+import NumberInput from "./number-input";
+import Output from "./output";
 
 export default class RateCalculator extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      rate: 10,
+      dayRate: 100,
+      hours: 10
+    };
+  }
+  dayRateChange = e => {
+    // console.log(e.target.value)
+    const value = parseInt(e.target.value, 0);
+    this.setState({
+      dayRate: value,
+      rate: value / this.state.hours
+    });
+  };
 
-    render() {
-        const rate = 10;
-        return (
-            <form>
-                <NumberInput id="day-rate" label="Day rate" min={0} max={5000} />
-                <NumberInput id="hours" label="Hours" min={1} max={12} />
-                <Output id="hourly-rate" label="Hourly rate" value={rate.toFixed(2)}
-/>
-            </form>
-        );
-    }
+  hourChange = e => {
+    const value = parseInt(e.target.value, 0);
+    this.setState({
+      hours: value,
+      rate: this.state.dayRate / value
+    });
+  };
+
+  render() {
+    // const rate = 10;
+    console.log(this.state);
+    return (
+      <form>
+        <NumberInput
+          onChange={this.dayRateChange}
+          id="day-rate"
+          value={this.state.dayRate}
+          label="Day rate"
+          min={0}
+          max={5000}
+        />
+        <NumberInput
+          onChange={this.hourChange}
+          id="hours"
+          label="Hours"
+          value={this.state.hours}
+          min={1}
+          max={12}
+        />
+        <Output
+          id="hourly-rate"
+          label="Hourly rate"
+          value={this.state.rate.toFixed(2)}
+        />
+      </form>
+    );
+  }
 }
