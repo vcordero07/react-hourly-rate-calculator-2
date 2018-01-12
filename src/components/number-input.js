47e41bd27bebcc96cdcac92fed67fce93bb2df24
vcordero07 <vcordero07@gmail.com>
2018-01-11 19:57:52 -0500

    wip

---
+++ src/components/number-input.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function NumberInput(props) {
+    return (
+        <div className="form-group">
+            <label htmlFor={props.id}>{props.label}</label>
+            <input type="number" id={props.id} min={props.min} max={props.max} />
+        </div>
+    );
+}
